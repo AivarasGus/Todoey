5d09f52c0209b4ebe6ee3f162728adaabfd524be
Aivaras <aivarasgust@gmail.com>
2019-06-14 21:27:33 +0300

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Aivaras Gustys on 14/06/2019.
+//  Copyright © 2019 Aivaras Gustys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
